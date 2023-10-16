da4136bea14e1ca0b9226dfae5fbad897a11ef1c
Reshna <reshnash@buffalo.edu>
2023-10-16 11:21:19 -0400

    Make changes as suggested by pylint.

--- projectile_unit_test.py
+++ projectile_unit_test.py
@@ -1,9 +1,26 @@
 import unittest
-from projectile_motion_with_air_resistance import calculate_horizontal_distance 
+from projectile_motion_with_air_resistance import calculate_horizontal_distance
+
 class TestCalculateHorizontalDistance(unittest.TestCase):
+    """
+    This class contains unit tests for the calculate_horizontal_distance function.
+    """
 
     def test_distance_calculation(self):
-        # Test case 1
+        """
+        Test the distance calculation using the calculate_horizontal_distance function.
+
+        Test case 1:
+        x0 = 1.0
+        y0 = 0.0
+        v0 = 10.0
+        launch_angle_deg = 45.0
+        m = 0.1
+
+        The expected_distance is 11.28, which is the corrected expected result for these inputs.
+
+        Calculate the actual result and assert that it matches the expected result.
+        """
         x0 = 1.0
         y0 = 0.0
         v0 = 10.0
@@ -17,7 +34,6 @@ class TestCalculateHorizontalDistance(unittest.TestCase):
 
         # Assert that the actual result matches the expected result
         self.assertAlmostEqual(actual_distance, expected_distance, places=1)
-
 
 if __name__ == '__main__':
     unittest.main()
