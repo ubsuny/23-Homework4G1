63fb9444c438219b88742de540f524728f4dfb3e
Mosaddeq Hossain <66480280+Mosaddeq107@users.noreply.github.com>
2023-10-18 12:25:30 -0400

    Update projectile_motion_with_air_resistance.py

--- projectile_motion_with_air_resistance.py
+++ projectile_motion_with_air_resistance.py
@@ -9,6 +9,8 @@ AIR_RESISTANCE = 0.1   # Air resistance constant (kg/s)
 # v0: Initial velocity (m/s) of the projectile
 # launch_angle_deg: Launch angle in degree 
 # m is mass.
+
+#Function to calculate the distance
 def calculate_horizontal_distance(x0, y0, v0, launch_angle_deg, m):
     # Convert launch angle to radians
     theta = np.radians(launch_angle_deg)
